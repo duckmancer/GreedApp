651d60ae2607a759bb4943ffdb8ad272632beb15
duck2 <wunderly.emma@gmail.com>
2022-12-07 09:03:32 -0800

    Started messing around with Trees.

--- GreedApp/UI.gd
+++ GreedApp/UI.gd
@@ -1,9 +1,18 @@
 extends Control
 
-onready var item_list = $ItemList
+onready var item_list := $ItemList
+onready var tree := $Tree
 
 var items = ["Passive", "Primary", "Secondary", "Special"]
+var tooltips = ["Always active", "Once per turn", "Once per turn", "Once per battle"]
 
 func _ready() -> void:
-	for i in items:
-		item_list.add_item(i);
+	var root = tree.create_item()
+	tree.set_hide_root(true)
+	for i in items.size():
+		var category = tree.create_item()
+		category.set_text(0, items[i])
+		var child = tree.create_item(category)
+		child.set_text(0, tooltips[i])
+		
+		
