16c977828d5117d74780b46e1af304e5a8457486
duck2 <wunderly.emma@gmail.com>
2022-12-19 12:58:46 -0800

    Added StatDisplay and renamed old version to PointDisplay.

--- GreedApp/StatDisplay.gd
+++ GreedApp/StatDisplay.gd
@@ -1,58 +1,30 @@
-extends LineEdit
+extends Label
 
-export var base_label := ""
+export var stat_label := ""
 
-export var max_val := 0 setget set_max_val
-export var cur_val := 0 setget set_cur_val
-export var capped := true
+onready var menu_button := $MenuButton
 
-func update_stat_display():
-	var label = base_label
-	label += String(cur_val)
-	label += "/"
-	label += String(max_val)
-	text = label
-	
-func apply_stat_input(new_text := ""):
-	clear()
-	release_focus()
-	if '/' in new_text:
-		var splits = new_text.split('/', false)
-		if splits.size() >= 2:
-			new_text = splits[0]
-			set_max_val(int(splits[1]))
-	var new_val = int(new_text)
-	if '+' in new_text or '-' in new_text:
-		new_val = cur_val + new_val
-	if new_val != 0 or '0' in new_text:
-		set_cur_val(new_val)
-	else:
-		update_stat_display()
+var stat_val := 0 setget set_stat_val
 
-func set_cur_val(new_val):
-	cur_val = new_val
-	if capped:
-# warning-ignore:narrowing_conversion
-		cur_val = min(max_val, new_val)
-# warning-ignore:narrowing_conversion
-	cur_val = max(0, cur_val)
-	update_stat_display()
-	
-func set_max_val(new_max_val):
-	max_val = new_max_val
-	update_stat_display()
+var modifier_options = [
+	"+1 Turn",
+	"+1 Round",
+	"+1 Battle",
+]
+
+func set_stat_val(new_val):
+	stat_val = new_val
+	text = stat_label + ": " + String(stat_val)
 
 func _ready() -> void:
-	update_stat_display()
+	var popup = menu_button.get_popup()
+	for option in modifier_options:
+		popup.add_item(option)
+	popup.add_font_override("font", preload("res://RobotoFont.tres"))
+	popup.connect("id_pressed", self, "_on_popup_id_pressed")
+	
+	set_stat_val(stat_val)
 
 
-func _on_StatDisplay_text_entered(new_text: String) -> void:
-	apply_stat_input(new_text)
-
-
-func _on_StatDisplay_focus_entered() -> void:
-	clear()
-
-
-func _on_StatDisplay_focus_exited() -> void:
-	apply_stat_input(text)
+func _on_popup_id_pressed(id):
+	set_stat_val(stat_val + 1)
